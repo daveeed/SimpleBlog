ea0649790612d382920ea1ebbb97836dbdba4e5e
Pascal Robert <probert@macti.ca>
2011-06-06 22:52:46 -0400

    Added security to non-HTML REST requests. Moved login logic to Person class

--- SimpleBlog/Sources/com/wowodc/rest/controllers/BaseRestController.java
+++ SimpleBlog/Sources/com/wowodc/rest/controllers/BaseRestController.java
@@ -1,13 +1,22 @@
 package com.wowodc.rest.controllers;
 
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import sun.misc.BASE64Decoder;
+
 import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WORequest;
+import com.webobjects.appserver.WOResponse;
+import com.wowodc.model.Person;
 
 import er.rest.format.ERXRestFormat;
 import er.rest.routes.ERXDefaultRouteController;
 
 public class BaseRestController extends ERXDefaultRouteController {
 
+  private Person authenticatedUser;
+  
   public BaseRestController(WORequest request) {
     super(request);
   }
@@ -46,10 +55,63 @@ public class BaseRestController extends ERXDefaultRouteController {
   protected ERXRestFormat defaultFormat() {
     return ERXRestFormat.json();
   }
-  
+
   @Override
   protected boolean isAutomaticHtmlRoutingEnabled() {
     return true;
   }
+
+  protected void initAuthentication() throws NotAuthorizedException {
+    String authValue = request().headerForKey( "authorization" );
+
+    if( authValue != null ) {
+      try {
+        byte[] authBytes = new BASE64Decoder().decodeBuffer( authValue.replace( "Basic ", "" ) );
+        String[] parts = new String( authBytes ).split( ":", 2 );
+        String username = parts[0];
+        String password = parts[1];
+        setAuthenticatedUser(Person.validateLogin(editingContext(), username, password));
+      } catch ( IOException e ) {
+        log.error( "Could not decode basic auth data: " + e.getMessage() );
+        e.printStackTrace();
+      }
+    } else {
+      throw new NotAuthorizedException();
+    }
+  }
+
+  protected Person authenticatedUser() {
+    return authenticatedUser;
+  }
+
+  @Override
+  public WOActionResults performActionNamed(String actionName, boolean throwExceptions)  {
+    if (!(ERXRestFormat.html().name().equals(this.format().name()))) {
+      try {
+        initAuthentication();
+      } catch (NoSuchElementException ex) {
+        WOResponse response = (WOResponse)errorResponse("Unauthorized", 401);
+        response.setHeader("Basic realm=\"Members\"", "WWW-Authenticate");
+        return response;
+      } catch (NotAuthorizedException ex) {
+        WOResponse response = (WOResponse)errorResponse("Unauthorized", 401);
+        response.setHeader("Basic realm=\"Members\"", "WWW-Authenticate");
+        return response;
+      }
+    }
+    return super.performActionNamed(actionName, throwExceptions);
+  }
+
+  protected void setAuthenticatedUser(Person authenticatedUser) {
+    this.authenticatedUser = authenticatedUser;
+  }
   
+  public class NotAuthorizedException extends Exception {
+
+    public NotAuthorizedException() {
+      super();
+    }
+
+  }
+
 }
--- SimpleBlog/Sources/com/wowodc/rest/controllers/BlogEntryController.java
+++ SimpleBlog/Sources/com/wowodc/rest/controllers/BlogEntryController.java
@@ -1,11 +1,38 @@
 package com.wowodc.rest.controllers;
 
+import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WORequest;
+import com.wowodc.model.BlogCategory;
+import com.wowodc.model.BlogEntry;
+
+import er.extensions.eof.ERXKeyFilter;
 
 public class BlogEntryController extends BaseRestController {
 
   public BlogEntryController(WORequest request) {
     super(request);
+  }
+  
+  @Override
+  public WOActionResults createAction() throws Throwable {
+    BlogEntry entry = create(filter());
+    editingContext().saveChanges();
+    return response(entry, null);
+  }
+  
+  @Override
+  public WOActionResults updateAction() throws Throwable {
+    BlogEntry entry = routeObjectForKey("blogEntry");
+    update(entry, null);
+    editingContext().saveChanges();
+    return response(entry, null);
+  }
+  
+  protected ERXKeyFilter filter() {
+    ERXKeyFilter filter = ERXKeyFilter.filterWithAttributes();
+    filter.include(BlogEntry.CATEGORIES.dot(BlogCategory.NAME));
+    filter.include(BlogEntry.CATEGORIES.dot(BlogCategory.SHORT_NAME));
+    return filter;
   }
   
 }
