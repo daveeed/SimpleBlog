b9d9f98e20bfefe00af5d168ff53c0c2fd8a4af0
David Holt <programmingosx@mac.com>
2011-07-05 18:50:35 -0700

    Added component to illustrate how to embed a non-d2w component in ERModernLook pagewrapper
    Signed-off-by: David Holt <programmingosx@mac.com>

---
+++ SimpleBlog/Sources/com/wowodc/ui/components/Instructions.java
@@ -0,0 +1,11 @@
+package com.wowodc.ui.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.pages.templates.ERD2WInspectPageTemplate;
+
+public class Instructions extends ERD2WInspectPageTemplate {
+  public Instructions(WOContext context) {
+    super(context);
+  }
+}
