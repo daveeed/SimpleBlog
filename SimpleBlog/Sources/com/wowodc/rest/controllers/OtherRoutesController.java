ce53346ab95674d71c8d4bcbd1d1d7bd9e2759d2
Pascal Robert <probert@macti.ca>
2011-11-18 17:27:54 -0500

    Default page is now the blob entries listing

---
+++ SimpleBlog/Sources/com/wowodc/rest/controllers/OtherRoutesController.java
@@ -0,0 +1,29 @@
+package com.wowodc.rest.controllers;
+
+import com.webobjects.appserver.WOActionResults;
+import com.webobjects.appserver.WORequest;
+import com.wowodc.ui.components.BlogEntryIndexPage;
+import com.wowodc.ui.components.Main;
+
+import er.rest.format.ERXRestFormat;
+
+public class OtherRoutesController extends BaseRestController {
+
+  public OtherRoutesController(WORequest request) {
+    super(request);
+  }
+  
+  public WOActionResults mainPageAction() {
+    return pageWithName(BlogEntryIndexPage.class);
+  }
+  
+  public WOActionResults adminPageAction() {
+    return pageWithName(Main.class);
+  }
+  
+  @Override
+  protected ERXRestFormat defaultFormat() {
+    return ERXRestFormat.html();
+  }
+
+}
